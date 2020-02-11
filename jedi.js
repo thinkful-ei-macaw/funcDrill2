2b89673461a162f907d2f5222807895844588acf
Douglas Miner <curr3nas@gmail.com>
2020-02-11 08:33:19 -0600

    fixed switch function

--- jedi.js
+++ jedi.js
@@ -1,7 +1,9 @@
+/* eslint-disable no-console */
+/* eslint-disable strict */
 // first function
 
 function jediName (firstName, lastName) {
-    console.log(firstName.substring(0, 3) + lastName.substring(0, 2));
+  console.log(firstName.substring(0, 3) + lastName.substring(0, 2));
 }
 
 jediName('Douglas', 'Miner');
@@ -9,15 +11,15 @@ jediName('Douglas', 'Miner');
 // second function
 
 function beyond(num) {
-    if (num === Infinity) {
-        console.log('And beyond')
-    } else if (num !== Infinity && num > 0) {
-        console.log('To infinity')
-    } else if (num !== Infinity && num <0) {
-        console.log('To negative infinity')
-    } else if (num === 0) {
-        console.log('Staying home');
-    }
+  if (num === Infinity) {
+    console.log('And beyond');
+  } else if (num !== Infinity && num > 0) {
+    console.log('To infinity');
+  } else if (num !== Infinity && num <0) {
+    console.log('To negative infinity');
+  } else if (num === 0) {
+    console.log('Staying home');
+  }
 }
 
 beyond(Infinity);
@@ -25,17 +27,17 @@ beyond(Infinity);
 // third function
 
 function decode(word) {
-    if (word[0] === 'a') {
-        console.log(word[1]);
-    } else if (word[0] === 'b') {
-        console.log(word[2]);
-    } else if (word[0] === 'c') {
-        console.log(word[3]);
-    } else if (word[0] === 'd') {
-        console.log(word[4]);
-    } else {
-        console.log(' ');
-    }
+  if (word[0] === 'a') {
+    console.log(word[1]);
+  } else if (word[0] === 'b') {
+    console.log(word[2]);
+  } else if (word[0] === 'c') {
+    console.log(word[3]);
+  } else if (word[0] === 'd') {
+    console.log(word[4]);
+  } else {
+    console.log(' ');
+  }
 }
 
 decode('craft');
@@ -52,53 +54,33 @@ decode('droop');
 // fourth function
 
 function howManyDays(month, leapYear) {
-    switch (month, leapYear) {
-        case 'January', false:
-        case 'March', false:
-        case 'May', false:
-        case 'July', false:
-        case 'August', false:
-        case 'October', false:
-        case 'December', false: {
-            result = `${month} has 31 days`;
-            break;
-        }
-        case 'April', false:
-        case 'June', false:
-        case 'September', false:
-        case 'November', false: {
-            result = `${month} has 30 days`;
-            break;
-        }
-        case 'February', false: {
-            result = `${month} has 28 days`;
-            break;
-        }   
-        case 'January', true:
-        case 'March', true:
-        case 'May', true:
-        case 'July', true:
-        case 'August', true:
-        case 'October', true:
-        case 'December', true: {
-            result = `${month} has 31 days`;
-            break;
-        }
-        case 'April', true:
-        case 'June', true:
-        case 'September', true:
-        case 'November', true: {
-            result = `${month} has 30 days`;
-            break;
-        }
-        case 'February', true: {
-            result = `${month} has 29 days`;
-            break;
-        }
-        console.log(result);
-    }
+  let days = 0;
+  switch (month) {
+  case 'January':
+  case 'March':
+  case 'May':
+  case 'July':
+  case 'August':
+  case 'October':
+  case 'December':
+    days = 31;
+    break;
+  case 'April':
+  case 'June':
+  case 'September':
+  case 'November':
+    days = 30;
+    break;
+  case 'February':
+    days = 28;
+    break;
+  }
+  days = days === 28 && leapYear ? 29 : days;
+
+  console.log(`${month} has ${days} days`);
 }
 
-howManyDays('February', true)
+howManyDays('February', true);
 
 //fifth function
+
